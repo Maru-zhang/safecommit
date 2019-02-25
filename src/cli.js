99c6b0c8c183dbc0c7d58c36714f7691b6b58b1b
Maru <zhangbinhui@souche.com>
2019-02-25 11:01:29 +0800

    feat: 添加版本号查看

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
+const program = require('commander');
 const Worker = require('./index');
+
+program
+  .version('0.0.1', '-v, --version')
+  .option('-f, --fuck', '查看当前的版本')
+  .parse(process.argv);
 
 const worker = new Worker();
 worker.run();
