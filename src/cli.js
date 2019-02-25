38711d35530915ea8784505ea5b70b7ba9612991
Maru <zhangbinhui@souche.com>
2019-02-25 11:18:17 +0800

    style: Trailing spaces not allowed

--- src/cli.js
+++ src/cli.js
@@ -2,13 +2,12 @@
 const program = require('commander');
 const Worker = require('./index');
 
+const worker = new Worker();
 program
   .version('0.0.1', '-v, --version')
   .option('-f, --fuck', '查看当前的版本')
   .option('-s, --reset', '重置当前的safecommit配置')
   .parse(process.argv);
-  
-const worker = new Worker();
 if (program.reset) {
   worker.reset();
 } else {
