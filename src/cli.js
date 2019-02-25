3cbeb91d4d237e0ad6f0015a380525a4ae5eb5f7
Maru <zhangbinhui@souche.com>
2019-02-25 11:20:59 +0800

    fix: 删除无用的cli选项

--- src/cli.js
+++ src/cli.js
@@ -5,7 +5,6 @@ const Worker = require('./index');
 const worker = new Worker();
 program
   .version('0.0.1', '-v, --version')
-  .option('-f, --fuck', '查看当前的版本')
   .option('-s, --reset', '重置当前的safecommit配置')
   .parse(process.argv);
 if (program.reset) {
