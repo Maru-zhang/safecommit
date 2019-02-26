388ac623c29f26849f30d2e7754729c38e8caf38
Maru <zhangbinhui@souche.com>
2019-02-26 19:44:33 +0800

    chore: Bump version

--- src/cli.js
+++ src/cli.js
@@ -4,7 +4,7 @@ const Worker = require('./index');
 
 const worker = new Worker();
 program
-  .version('1.0.0', '-v, --version')
+  .version('1.0.2', '-v, --version')
   .option('-s, --reset', '重置当前的safecommit配置')
   .option('-c, --swift-config <path>', '配置全局lint配置文件')
   .parse(process.argv);
